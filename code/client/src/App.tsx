fb142e9fb7a97bbfc0ddeefc63c3e30b160d6e51
Aryan Gupta <aryang10@illinois.edu>
2025-04-06 04:25:24 -0500

    added radio button and text-field for feedbacks

--- code/client/src/App.tsx
+++ code/client/src/App.tsx
@@ -8,6 +8,8 @@ function App() {
   const [message, setMessage] = useState('');
   const [audioUrl, setAudioUrl] = useState<string | null>(null);
   const audioRef = useRef<HTMLAudioElement | null>(null);
+  const [showProfanity, setShowProfanity] = useState(false);
+  const [profanityData, setProfanityData] = useState<any[]>([]);
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const selectedFile = event.target.files?.[0];
@@ -41,6 +43,12 @@ function App() {
       if (response.ok) {
         setResults(data.results);
         setMessage(data.message);
+        console.log(data);
+        const profanityRes = await fetch('../../../src/tests/results/profanity_report.json');
+        console.log(profanityRes)
+        const profanityJson = await profanityRes.json();
+        console.log(profanityJson)
+        setProfanityData(profanityJson);
       } else {
         setMessage(data.error || 'Analysis failed');
       }
@@ -62,20 +70,21 @@ function App() {
       {message && <p>{message}</p>}
       {audioUrl && results.length > 0 && (
         <div style={{ marginTop: '40px', padding: '20px', borderTop: '2px solid #ddd', width: '90%', marginLeft: 'auto', marginRight: 'auto' }}>
-          {/* <h3 style={{ textAlign: 'center' }}>🎧 RESULTS </h3> */}
-          {/* <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', gap: '20px', marginBottom: '20px' }}> */}
-            {/* <button onClick={() => {
-              if (audioRef.current) audioRef.current.currentTime -= 5;
-            }} style={{ fontSize: '24px' }}>⏪</button>
-
-            <button onClick={() => audioRef.current?.play()} style={{ fontSize: '36px', borderRadius: '50%', width: '60px', height: '60px' }}>▶️</button>
-
-            <button onClick={() => {
-              if (audioRef.current) audioRef.current.currentTime += 5;
-            }} style={{ fontSize: '24px' }}>⏩</button> */}
-          {/* </div> */}
-
           <audio ref={audioRef} src={audioUrl} controls style={{ width: '100%' }} />
+          <div style={{ display: 'flex', marginTop: '20px' }}>
+            <div style={{ flex: 1 }}>
+              <label>
+                <input type="radio" checked={showProfanity} onChange={() => setShowProfanity(!showProfanity)} />
+                Profanity Check
+              </label>
+            </div>
+            <textarea
+              style={{ flex: 2, height: '100px', marginLeft: '20px' }}
+              placeholder="Comments"
+              readOnly
+              value={showProfanity ? '🔴 Profanity detected. Feedback will appear here.' : ''}
+            />
+          </div>
         </div>
       )}
     </div>
